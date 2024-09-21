a5af4fcbb15d2391e46aa4afbe710cf6f7889f2d
Vinicius Sonntag Dorow <vinicius.s.dorow@gmail.com>
2024-09-21 18:54:26 -0300

    unzip function

--- dags/credito_rural_dag.py
+++ dags/credito_rural_dag.py
@@ -2,7 +2,9 @@
 from airflow.decorators import dag, task
 from pendulum import datetime
 import requests
-import zipfile
+import gzip
+import shutil
+import os
 
 @dag(
     start_date=datetime(2024, 1, 1),
@@ -18,12 +20,20 @@ def credito_rural():
         for ano in anos:
             url = f"https://www.bcb.gov.br/htms/sicor/DadosBrutos/SICOR_CONTRATOS_MUNICIPIO_{ano}.gz"
             r = requests.get(url)
-            path = f"C:/Users/vinic/OneDrive/Documentos/python/airflow_dbt_pipeline/include/{ano}.rar"
+            path = f"C:/Users/vinic/OneDrive/Documentos/python/airflow_dbt_pipeline/include/{ano}.gz"
             with open(path, "wb") as f:
                 f.write(r.content)
-        return path
-    
+        return path[0:71]
+
     @task()
+    def unzip(path):
+        for file in os.listdir(path):
+            if file.endswith(".gz"):
+                zip_url = os.path.join(path, file)
+                output_file = zip_url[:-3] + ".csv"
+                with gzip.open(zip_url, 'rb') as f_in:
+                    with open(output_file, 'wb') as f_out:
+                        shutil.copyfileobj(f_in, f_out)
 
 
 # Instantiate the DAG
